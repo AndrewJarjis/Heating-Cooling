732213eb1fdfe94eec3ff8ef2d6e66ed17ab8950
andrewjarjis <andrewjarjis56@gmail.com>
2023-03-08 18:00:55 -0500

    Changed first block of code

--- Homework.py
+++ Homework.py
@@ -1,19 +1,24 @@
 actualtemp = 75
 desiredtemp = 77
-if actualtemp > desiredtemp:
-    status = 'Run A/C'
-elif actualtemp < desiredtemp:
-    status = 'Run the heat'
-if actualtemp == desiredtemp:
-    status = 'Stand by'
-print('Heating and cooling system status: ' + status)
+
+
+def status(actualTemp, desiredtemp):
+    if actualTemp > desiredtemp:
+        print('Run A/C')
+    elif actualTemp < desiredtemp:
+        print('Run the heat')
+    if actualTemp == desiredtemp:
+        print('Stand by')
+
+
+status(actualtemp, desiredtemp)
 
 
 def convert_temperature(tempCelsius, targetUnit):
     if targetUnit == 'C':
         print(f'{tempCelsius} C')
     elif targetUnit == 'F':
-        tempFahrenheit = (tempCelsius * 9/5) + 32
+        tempFahrenheit = (tempCelsius * 9 / 5) + 32
         print(f'{tempFahrenheit} F')
     elif targetUnit == 'K':
         tempKelvin = tempCelsius + 273.15
@@ -21,9 +26,8 @@ def convert_temperature(tempCelsius, targetUnit):
     else:
         print(f'{tempCelsius} {targetUnit}')
 
+
 convert_temperature(25, 'C')
 convert_temperature(25, 'F')
 convert_temperature(25, 'K')
 convert_temperature(25, 'X')
-
-
